b4246ee5693dc601686b156207d7ec0be4572704
adriansatria <adriansatria.siswa@smkn4bdg.sch.id>
2021-02-04 13:39:26 +0700

    Latihan SQLite 1

---
+++ app/src/main/java/com/example/sqlite_latihan1/MainFragment.java
@@ -0,0 +1,99 @@
+package com.example.sqlite_latihan1;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainFragment extends Fragment implements View.OnClickListener, RecyclerviewAdapter.OnUserClickListener {
+    RecyclerView recyclerView;
+    EditText editName, editAge;
+    Button btnSubmit;
+    RecyclerView.LayoutManager layoutManager;
+    Context context;
+    List<PersonBean> listPersonInfo;
+
+    public MainFragment(){
+        // Empty Constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getActivity();
+        recyclerView = view.findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+
+        editName = view.findViewById(R.id.editname);
+        editAge = view.findViewById(R.id.editage);
+        btnSubmit = view.findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(this);
+        setupRecyclerView();
+    }
+
+    public void setupRecyclerView(){
+        DatabaseHelper db = new DatabaseHelper(context);
+        listPersonInfo = db.selectUserData();
+        RecyclerviewAdapter adapter = new RecyclerviewAdapter(context, listPersonInfo, this);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnSubmit){
+            DatabaseHelper db = new DatabaseHelper(context);
+            PersonBean currentPerson = new PersonBean();
+            String btnStatus = btnSubmit.getText().toString();
+
+            if(btnStatus.equals("SUBMIT")){
+                currentPerson.setName(editName.getText().toString());
+                currentPerson.setAge(Integer.parseInt(editAge.getText().toString()));
+                db.insert(currentPerson);
+            }else if(btnStatus.equals("Update")){
+                currentPerson.setName(editName.getText().toString());
+                currentPerson.setAge(Integer.parseInt(editAge.getText().toString()));
+                db.update(currentPerson);
+            }
+
+            setupRecyclerView();
+            editName.setText("");
+            editAge.setText("");
+            editName.setFocusable(true);
+            btnSubmit.setText("SUBMIT");
+        }
+    }
+
+    @Override
+    public void onUserClick(PersonBean currentPerson, String action) {
+        if(action.equals("Edit")){
+            editName.setText(currentPerson.getName());
+            editName.setFocusable(false);
+            editAge.setText(currentPerson.getAge() + "");
+            btnSubmit.setText("Update");
+        }else if(action.equals("Delete")){
+            DatabaseHelper db = new DatabaseHelper(context);
+            db.delete(currentPerson.getName());
+            setupRecyclerView();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sqlite_latihan1/PersonBean.java
@@ -0,0 +1,26 @@
+package com.example.sqlite_latihan1;
+
+public class PersonBean {
+    private String name;
+    private int age;
+
+    public PersonBean(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
